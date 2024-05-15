fdd52d74c1f2d990e6af862c60ce8382642e7496
Rugroso <133253941+Rugroso@users.noreply.github.com>
2024-05-14 19:41:35 -0700

    Nuevos sprites y solucion de errores

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 5500 });
+const server = new WebSocket.Server({ port: 3000 });
 let players = [];
-
 server.on('connection', function(ws) {
     players.push(ws);
 
@@ -9,7 +8,7 @@ server.on('connection', function(ws) {
         try {
             console.log(`Received: ${message}`);
             const data = JSON.parse(message);
-            const toSend = JSON.stringify({ id: data.id, x: data.x, y: data.y });
+            const toSend = JSON.stringify({ id: data.id, x: data.x, y: data.y, dx:data.dx, dy:data.dy });
             players.forEach(client => {
                 if (client !== ws && client.readyState === WebSocket.OPEN) {
                     client.send(toSend);
